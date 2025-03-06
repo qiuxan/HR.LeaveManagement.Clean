8ca47a47ff922aeb3e891af81db043a487187a67
qiuxan <qiuxan@gmail.com>
2025-03-06 21:35:03 +1100

    Add Repository Implementations

--- src/Core/HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
+++ src/Core/HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
@@ -3,9 +3,9 @@
 public interface IGenericRepository<T> where T : class
 {
     //CRUD operations all entities to have
-    Task<List<T>> GetAsync();
+    Task<IReadOnlyList<T>> GetAsync();
     Task<T> GetByIdAsync(int id);
-    Task<T> CreateAsync(T entity);
-    Task<T> UpdateAsync(T entity);
-    Task<T> DeleteAsync(T entity);
+    Task CreateAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
 }
