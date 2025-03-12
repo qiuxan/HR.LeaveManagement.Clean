47025f90dc557a540bdeed7abcaad2117375f367
qiuxan <qiuxan@gmail.com>
2025-03-12 19:51:34 +1100

    Complete Leave Allocation Feature

---
+++ src/Core/HR.LeaveManagement.Application/Features/LeaveAllocation/Queries/GetLeaveAllocations/GetLeaveAllocationListHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocations;
+
+public class GetLeaveAllocationListHandler : IRequestHandler<GetLeaveAllocationListQuery, List<LeaveAllocationDto>>
+{
+    private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+    private readonly IMapper _mapper;
+
+    public GetLeaveAllocationListHandler(ILeaveAllocationRepository leaveAllocationRepository, IMapper mapper)
+    {
+        _leaveAllocationRepository = leaveAllocationRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<List<LeaveAllocationDto>> Handle(GetLeaveAllocationListQuery request, CancellationToken cancellationToken)
+    {
+        var allLeaveAllocations = await _leaveAllocationRepository.GetLeaveAllocationsWithDetails();
+        var leaveAllocationDto = _mapper.Map<List<LeaveAllocationDto>>(allLeaveAllocations);
+        return leaveAllocationDto;
+    }
+}
---
+++ src/Core/HR.LeaveManagement.Application/MappingProfiles/LeaveAllocationProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocations;
+using HR.LeaveManagement.Domain;
+using HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocationDetails;
+using HR.LeaveManagement.Application.Features.LeaveAllocations.Requests.Commands;
+
+namespace HR.LeaveManagement.Application.MappingProfiles;
+
+public class LeaveAllocationProfile: Profile
+{
+    public LeaveAllocationProfile()
+    {
+        CreateMap<LeaveAllocationDto, LeaveAllocation>().ReverseMap();
+        CreateMap<LeaveAllocation, LeaveAllocationDetailsDto>();
+        CreateMap<CreateLeaveAllocationCommand, LeaveAllocation>();
+        CreateMap<UpdateLeaveAllocationCommand, LeaveAllocation>();
+    }
+}
