1527ebd1c9de026725e7cc0f56bb66a67862c573
qiuxan <qiuxan@gmail.com>
2025-03-16 20:29:47 +1100

    Configure App for API

--- src/UI/HR.LeaveManagement.BlazorUI/Program.cs
+++ src/UI/HR.LeaveManagement.BlazorUI/Program.cs
@@ -1,11 +1,22 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using HR.LeaveManagement.BlazorUI;
+using HR.LeaveManagement.BlazorUI.Services.Base;
+using Microsoft.Extensions.DependencyInjection; // Add this using directive
+using System.Net.Http;
+using HR.LeaveManagement.BlazorUI.Contracts;
+using HR.LeaveManagement.BlazorUI.Services; // Add this using directive
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddHttpClient<IClient, Client>(client=>client.BaseAddress = new Uri("https://localhost:7276"));
+
+builder.Services.AddScoped<ILeaveAllocationService, LeaveAllocationService>();
+builder.Services.AddScoped<ILeaveRequestService, LeaveRequestService>();
+builder.Services.AddScoped<ILeaveTypeService, LeaveTypeService>();
 
 await builder.Build().RunAsync();
