881620b543176ae766d9a523521eb787daf8105e
qiuxan <qiuxan@gmail.com>
2025-03-18 21:46:00 +1100

    Setup Leave Type Management UI

--- src/UI/HR.LeaveManagement.BlazorUI/Models/LeaveTypes/LeaveTypeVM.cs
+++ src/UI/HR.LeaveManagement.BlazorUI/Models/LeaveTypes/LeaveTypeVM.cs
@@ -1,5 +1,17 @@
-﻿namespace HR.LeaveManagement.BlazorUI.Models.LeaveTypes;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
 
-public class LeaveTypeVM
+namespace HR.LeaveManagement.BlazorUI.Models.LeaveTypes
 {
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Default Number Of Days")]
+        public int DefaultDays { get; set; }
+    }
 }
